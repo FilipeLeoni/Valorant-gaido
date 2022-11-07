16f5ce79627861b7ba069f973c2f842f9885a358
FilipeLeoni <Filipe_sempre@outlook.com>
2022-11-07 16:10:55 -0300

    Agent Image Correction

--- pages/components/card.tsx
+++ pages/components/card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Agent } from "../entities/agent";
-
+import Image from "next/image";
+import { motion } from "framer-motion";
 interface Props {
   data: Agent;
 }
@@ -10,13 +11,16 @@ export const Card: React.FC<Props> = ({
 }) => {
   return (
     <>
-      <div className="w-[280px] h-[400px] bg-pink rounded-lg cursor-pointer ">
+      <motion.div
+        whileHover={{ scale: 1.1 }}
+        className="w-[280px] h-[400px] bg-pink rounded-lg cursor-pointer "
+      >
         <div>
           <div className="absolute w-[290px] ml-[-4px] opacity-50 mt-[-28px] flex justify-center items-center ">
-            <img src={background} />
+            <Image src={background} width="290px" height="366px" />
           </div>
           <div className="w-[400px] ml-[-53px] relative object-cover h-[500px]">
-            <img src={fullPortraitV2} />
+            <Image src={fullPortraitV2} width="400px" height="363px" />
           </div>
         </div>
         <div className="relative mt-[-310px] text-blue pt-28 bg-gradient-to-t rounded from-pink via-pink to-transparent justify-center flex">
@@ -24,11 +28,11 @@ export const Card: React.FC<Props> = ({
             <h2 className="text-xl font-semibold ">{displayName}</h2>
             <p className="mb-2 leading-4 text-sm">{role.displayName}</p>
             <div className="w-6">
-              <img src={role.displayIcon} />
+              <Image src={role.displayIcon} width="24px" height="24px" />
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
